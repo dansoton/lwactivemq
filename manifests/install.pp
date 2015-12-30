e392d67a5cb92227ea9c3ed73fbeb36c7f150d0c
John Cook <john.cook@lifeway.com>
2015-12-30 17:26:49 -0600

    adding jmx

--- manifests/install.pp
+++ manifests/install.pp
@@ -91,12 +91,12 @@ class lwactivemq::install (
     }
   } ->
 
-  if ($usejmx) {
+  if $usejmx {
     file_line { 'jmxsetup':
     path  => '/etc/default/activemq',
     line  => "ACTIVEMQ_SUNJMX_START=\"-Dcom.sun.management.jmxremote.port=1616 -Dcom.sun.management.jmxremote.access.file=${ACTIVEMQ_CONF}/jmx.access -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote\"",
     match => '^ACTIVEMQ_SUNJMX_START.*',
-  }
+    }
 
   }
 
